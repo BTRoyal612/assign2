bb82e359cc7047fc8a878f3724f2305b8682a993
bao <gb.hoang02@gmail.com>
2023-09-07 22:14:15 +0930

    Add throw Exception to all methods incase input is null, add method readFile

--- src/main/common/JSONHandler.java
+++ src/main/common/JSONHandler.java
@@ -6,9 +6,36 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class JSONHandler {
 
+    public static String readFile(String filePath) throws Exception {
+        if (filePath == null) {
+            throw new Exception("Error: filePath is null.");
+        }
+
+        StringBuilder content = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw new Exception("Error reading the file: " + e.getMessage());
+        }
+
+        return content.toString();
+    }
+
     public static JSONObject convertTextToJSON(String inputText) throws Exception {
+        if (inputText == null) {
+            throw new Exception("Input text is null.");
+        }
+
         String[] lines = inputText.split("\n");
         Map<String, Object> dataMap = new HashMap<>();
 
@@ -28,7 +55,11 @@ public class JSONHandler {
         return new JSONObject(dataMap);
     }
 
-    public static String convertJSONToText(JSONObject jsonObject) {
+    public static String convertJSONToText(JSONObject jsonObject) throws Exception {
+        if (jsonObject == null) {
+            throw new Exception("Error: jsonObject is null.");
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
 
         Iterator<String> keys = jsonObject.keys();
