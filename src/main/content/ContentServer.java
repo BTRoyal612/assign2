798686640ecfd978ff6c51cce2761bd11dd9ffb8
bao <gb.hoang02@gmail.com>
2023-09-12 02:25:07 +0930

    Implement complete JSONHandler without additional libraries, add function printJSON and parseJSON. Refactor ContentServer, GETClient and their testing for the new JSONHandler

--- src/main/content/ContentServer.java
+++ src/main/content/ContentServer.java
@@ -6,7 +6,6 @@ import main.network.NetworkHandler;
 import main.network.SocketNetworkHandler;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
